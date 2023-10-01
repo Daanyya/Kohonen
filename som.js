116325f8fc78fe197e462c7d3406838415a06686
Daanyya <103365572+Daanyya@users.noreply.github.com>
2023-10-02 01:43:28 +0400

    Add files via upload

--- som.js
+++ som.js
@@ -1,54 +1,47 @@
-//! USEFUL LINKS
-// https://medium.com/machine-learning-researcher/self-organizing-map-som-c296561e2117
-// https://codepen.io/raman-mamedov/pen/JjdNWqX?editors=0010
-// https://algowiki-project.org/ru/%D0%A1%D0%B0%D0%BC%D0%BE%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D1%83%D1%8E%D1%89%D0%B0%D1%8F%D1%81%D1%8F_%D0%BA%D0%B0%D1%80%D1%82%D0%B0_%D0%9A%D0%BE%D1%85%D0%BE%D0%BD%D0%B5%D0%BD%D0%B0_(%D0%B0%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%BA%D0%BB%D0%B0%D1%81%D1%82%D0%B5%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8)
-
-
-//! BASIC CONFIGURATION
+//Константы отображения карты
 const NUMBER_OF_INPUTS = 3;
-const NUMBER_OF_NEURONS_BY_WIDTH = 100;
-const NUMBER_OF_NEURONS_BY_HEIGHT = 100;
-const NEURON_SIZE = 7;
+const NUMBER_OF_NEURONS_BY_WIDTH = 50;
+const NUMBER_OF_NEURONS_BY_HEIGHT = 50;
+const NEURON_SIZE = 10;
 
-//! DATA SET
-// import { DATA_SET } from "./religion_dataset.js";
-// import { DATA_SET } from "./expected_lifetime_dataset.js";
-// import { DATA_SET } from "./suicide_rate_dataset.js";
-// import { DATA_SET } from "./random_dataset.js";
+//Dataset
 import { DATA_SET } from "./weather_dataset.js";
 
-//! MATH VARIABLES
-// it is T variable in theory of SOM
+//Константы и переменные
+//Число итераций
 const NUMBER_OF_TRAINING_STEPS = DATA_SET.length;
-// it is t variable in theory of SOM
+//Текущая итерация
 let numberOfTrainingStep = 0;
-// sigma is size of the neighborhood around the winner neuron 
+//Константа для расчёта функции распространения sigma
 const sigma0 = Math.max(NEURON_SIZE * NUMBER_OF_NEURONS_BY_WIDTH, NEURON_SIZE * NUMBER_OF_NEURONS_BY_HEIGHT) / 5;
+//Переменная для подсчёта распространения
 let sigma;
-// 
+//Константа для расчёта функции распространения sigma
 const lambda = NUMBER_OF_TRAINING_STEPS / Math.log(sigma0);
-// theta is influence rate
+//Переменная для подсчёта плотности обучения
 let theta;
+//Скорость обучения
 const learningRate0 = 0.1;
+//Переменная для подсчёта скорости обучения
 let learningRate;
 
-//! PAGE ELEMENTS
-// canvas
+//Элементы HTML
+//Canvas
 let mapElement = document.getElementById("map");
 mapElement.width = NEURON_SIZE * NUMBER_OF_NEURONS_BY_WIDTH;
 mapElement.height = NEURON_SIZE * NUMBER_OF_NEURONS_BY_HEIGHT;
 let context = mapElement.getContext("2d");
-// menu
+//Menu
 let menuElement = document.getElementById("menu");
 menuElement.style.height = `${NUMBER_OF_NEURONS_BY_HEIGHT * NEURON_SIZE}px`;
-// buttons
+//Buttons
 document.querySelector('#teach-button').addEventListener('click', () => {
   som.teach();
 });
 document.querySelector('#teach-all-button').addEventListener('click', () => {
   som.fullTeach();
 });
-document.querySelector('#teach-0-button').addEventListener('click', () => {
+/*document.querySelector('#teach-0-button').addEventListener('click', () => {
   som.renderForParam(0);
 });
 document.querySelector('#teach-1-button').addEventListener('click', () => {
@@ -56,81 +49,82 @@ document.querySelector('#teach-1-button').addEventListener('click', () => {
 });
 document.querySelector('#teach-2-button').addEventListener('click', () => {
   som.renderForParam(2);
-});
+});*/
 document.querySelector('#find-winner-for-cusom-input').addEventListener('click', () => {
   let data = JSON.parse(document.querySelector('#custom-input').value.replace(/'/g, '"'));
   let winner = som.findWinner(data);
   som.showNeuron(winner, "black");
 });
 
-//! NEURON
+//Класс нейрона
 class Neuron {
   constructor(x_coordinate, y_coordinate) {
+    //Координата по X
     this.x_coordinate = x_coordinate;
+    //Координата по Y
     this.y_coordinate = y_coordinate;
-    // random value should be in |w| <= 1/sqrt(NUMBER_OF_INPUTS)
-    // basic formula is Math.random() * (max - min) + min;
+    //Массив весов для каждого входного сигнала
     this.weights = Array.from({length: NUMBER_OF_INPUTS}, () => Math.random() * ((1 / Math.sqrt(NUMBER_OF_INPUTS)) + (1 / Math.sqrt(NUMBER_OF_INPUTS))) + (-(1 / Math.sqrt(NUMBER_OF_INPUTS))));
   }
   //? function finding the average of all weight of neuron weights
   //? use to color map in grayscale like rgb(average*255, average*255, average*255)
-  getAverageWeight() {
+  /*getAverageWeight() {
     let averageWeight = 0;
     for (let i = 0; i < this.weights.length; i++) {
       averageWeight += this.weights[i];
     }
     // should add (1 / Math.sqrt(NUMBER_OF_INPUTS)) to make the range like 0..1 not like -(1 / Math.sqrt(NUMBER_OF_INPUTS))..(1 / Math.sqrt(NUMBER_OF_INPUTS)) 
     return averageWeight / this.weights.length + (1 / Math.sqrt(NUMBER_OF_INPUTS));
-  }
+  }*/
 }
 
-//! SELF ORGANIZING MAP
+//Класс карты
 class SOM {
   constructor() {
-    // array contains all neurons
+    //Массив всех нейронов
     this.map = new Array(NUMBER_OF_NEURONS_BY_WIDTH * NUMBER_OF_NEURONS_BY_HEIGHT);
     for (let i = 0; i < this.map.length; i++) {
-      // coordinates calculation
+      //Координаты по X, Y
       let x_coordinate = Math.floor(i / NUMBER_OF_NEURONS_BY_WIDTH);
       let y_coordinate = i % NUMBER_OF_NEURONS_BY_WIDTH;
-      // neuron initialisation
+      //Инициализация нейрона по коодинатам
       this.map[i] = new Neuron(x_coordinate, y_coordinate);
     }
   }
-  //? fucntion displaying map with colors
+  //Отрисовка всех нейронов на карте
   render() {
-    // clear canvas
+    //Очистка карты
     context.clearRect(0,0,mapElement.width,mapElement.height);
-    // draw all neurons
+    //Отрисовка нейронов на карте по координатам
     for (let i = 0; i < this.map.length; i++) {
-      //* use grayscale if number over inputs more than three
-      // context.fillStyle = `rgb(${this.map[i].getAverageWeight() * 255}, ${this.map[i].getAverageWeight() * 255}, ${this.map[i].getAverageWeight() * 255})`;
+      //Заливка нейрона
       context.fillStyle = `rgb(${this.map[i].weights[0] * 255}, ${this.map[i].weights[1] * 255}, ${this.map[i].weights[2] * 255})`;
-      // x_coordinate = height, y_coordinate = width
       context.fillRect(this.map[i].y_coordinate * NEURON_SIZE, this.map[i].x_coordinate * NEURON_SIZE, NEURON_SIZE, NEURON_SIZE);
     }
   }
-  //? function finding and returning winner neuron
+  //Поиск победителя
   findWinner(input) {
-    // this array contains the Euclidean distance from the current input data to each neuron
+    //Массив расстояний для каждого нейрона
     let distancesMap = new Array(NUMBER_OF_NEURONS_BY_WIDTH * NUMBER_OF_NEURONS_BY_HEIGHT);
+    //Нейрон с минимальным расстоянием
     let minDistanceIndex = 0;
+    //По каждому расстоянию для нейронов
     for (let i = 0; i < distancesMap.length; i++) {
-      // Euclidean distance calculation
+      //По формуле подсчёта расстояния
       let sum = 0;
       for (let j = 0; j < NUMBER_OF_INPUTS; j++) {
         sum += (input[j] - this.map[i].weights[j])**2;
       }
       distancesMap[i] = Math.sqrt(sum);
-      // min distance searching 
+      //Проверка на минимальное расстояние
       if (distancesMap[i] < distancesMap[minDistanceIndex]) {
         minDistanceIndex = i;
       }
     }
-    // return win neuron
+    //Индекс нейрона победителя
     return this.map[minDistanceIndex];
   }
-  //? function displaying the input neuron colored in a given color
+  /*
   showNeuron(neuron, color) {
     context.fillStyle = color;
     context.fillRect(neuron.y_coordinate * NEURON_SIZE, neuron.x_coordinate * NEURON_SIZE, NEURON_SIZE, NEURON_SIZE);
@@ -140,42 +134,47 @@ class SOM {
     for (let i = 0; i < DATA_SET.length; i++) {
       this.showNeuron(this.findWinner(DATA_SET[i]), "red");
     }
-  }
-  //? the network training on the whole dataset function for 
+  }*/
+  //Обучение 
   fullTeach() {
     while (DATA_SET.length > 0) {
       this.teach();
     }
     this.render();
   }
+  //Обучение
   teach() {
-    // get random input and delete it drom dataset
+    //Пока в dataset ещё остались объекты
     if (DATA_SET.length > 0) {
+      //Выбор случайного объекта из dataset
       let randomInputIndex = Math.floor(Math.random() * DATA_SET.length);
       let randomInput = DATA_SET[randomInputIndex];
+      //Удаление объекта из dataset
       DATA_SET.splice(randomInputIndex,1);
-      // find winner neuron
+      //Поиск нейрона победителя
       let winnerNeuron = this.findWinner(randomInput);
       this.showNeuron(winnerNeuron, "blue");
-      // find all nearby neurons for winner neuron
+      //Поиск нейронов соседей нейрона победителя
       sigma = sigma0 * Math.exp(-(numberOfTrainingStep / lambda));
       let nearbyMap = [];
       for (let i = 0; i < this.map.length; i++) {
-        // find distances between the winner neuron and the others
+        //Определение расстояния нейрона до нейрона победителя
         let distance = Math.sqrt((this.map[i].x_coordinate - winnerNeuron.x_coordinate)**2 + (this.map[i].y_coordinate - winnerNeuron.y_coordinate)**2);
-        // save only nearby neurons
+        //Если расстояние меньше sigma
         if (distance < sigma) {
+          //Добавление нейрона соседа в массив
           nearbyMap.push(this.map[i]);
         }
       }
-      // iterate over all nearby neurons
+      //Корректировка весов
       for (let i = 0; i < nearbyMap.length; i++) {
-        // find the distance between winner neuron and nearby
+        //Определение расстояния нейрона до нейрона победителя
         let distance =  Math.sqrt((nearbyMap[i].x_coordinate - winnerNeuron.x_coordinate)**2 + (nearbyMap[i].y_coordinate - winnerNeuron.y_coordinate)**2);
-        // calculation theta and learning rate
+        //Рассчёт плотности обучения для нейрона
         theta = Math.exp(-(distance**2 / (2 * sigma**2)));
+        //Рассчёт скорости обучения для нейрона
         learningRate = learningRate0 * Math.exp(-(numberOfTrainingStep / lambda));
-        // update nearby neuron weights
+        //Корректировка весов  соседей
         for (let j = 0; j < nearbyMap[i].weights.length; j++) {
           nearbyMap[i].weights[j] += theta * learningRate * (randomInput[j] - nearbyMap[i].weights[j]);
         }
@@ -184,7 +183,7 @@ class SOM {
       numberOfTrainingStep++;
     }
   }
-  //? render grayscale map for each input parameter
+  /*? render grayscale map for each input parameter
   renderForParam(p) {
     context.clearRect(0,0,mapElement.width,mapElement.height);
     // draw all neurons
@@ -193,7 +192,7 @@ class SOM {
       // x_coordinate = height, y_coordinate = width
       context.fillRect(this.map[i].y_coordinate * NEURON_SIZE, this.map[i].x_coordinate * NEURON_SIZE, NEURON_SIZE, NEURON_SIZE);
     }
-  }
+  }*/
 }
 
 let som = new SOM();
